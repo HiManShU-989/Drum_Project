a57b037454d845ecf616e4167f638e1b8a8ccd3f
Himanshu Singh <124182328+HiManShU-989@users.noreply.github.com>
2023-06-09 23:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,45 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         makeSound(buttonInnerHTML);
     });
 }
-document.addEventListner("keypress",function(event){
+
+document.addEventListener("keypress", function (event) {
     makeSound(event.key);
 });
-function makeSound(key){
+function makeSound(key) {
     switch (key) {
-            case "w":
-                var audio = new Audio("tom-1.mp3");
-                audio.play();
-                break;
-            case "a":
-                var audio = new Audio("tom-2.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio("tom-3.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio("tom-4.mp3");
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio("snare.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio("kick-bass.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio("crash.mp3");
-                audio.play();
-                break;
-            default:
-                alert("Beat the Drums!!!!");
-                break;
-        }
+        case "w":
+            var audio = new Audio("tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("crash.mp3");
+            audio.play();
+            break;
+        default:
+            alert("Beat the Drums!!!!");
+            break;
+    }
 }
-
-
 
 
 
