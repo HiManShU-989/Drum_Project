cf8776eb85ba24838dee04e2b6a6630143690b35
Himanshu Singh <124182328+HiManShU-989@users.noreply.github.com>
2023-06-10 12:30:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keypress", function (event) {
     makeSound(event.key);
+     buttonAnimation(event.key);
 });
 function makeSound(key) {
     switch (key) {
@@ -43,7 +45,13 @@ function makeSound(key) {
             break;
     }
 }
-
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
 
 
 
