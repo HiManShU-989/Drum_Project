17bbdad9c6275c2307cb71842c08b3e8a37004f8
Himanshu Singh <124182328+HiManShU-989@users.noreply.github.com>
2024-05-28 19:24:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,31 +14,31 @@
   function makeSound(key) {
     switch (key) {
       case 'w':
-        let tom1 = new Audio('sounds/tom-1.mp3');
+        let tom1 = new Audio('tom-1.mp3');
         tom1.play();
         break;
       case 'a':
-        let tom2 = new Audio('sounds/tom-2.mp3');
+        let tom2 = new Audio('tom-2.mp3');
         tom2.play();
         break;
       case 's':
-        let tom3 = new Audio('sounds/tom-3.mp3');
+        let tom3 = new Audio('tom-3.mp3');
         tom3.play();
         break;
       case 'd':
-        let tom4 = new Audio('sounds/tom-4.mp3');
+        let tom4 = new Audio('tom-4.mp3');
         tom4.play();
         break;
       case 'j':
-        let snare = new Audio('sounds/snare.mp3');
+        let snare = new Audio('snare.mp3');
         snare.play();
         break;
       case 'k':
-        let crash = new Audio('sounds/crash.mp3');
+        let crash = new Audio('crash.mp3');
         crash.play();
         break;
       case 'l':
-        let kick = new Audio('sounds/kick-bass.mp3');
+        let kick = new Audio('kick-bass.mp3');
         kick.play();
         break;
       default:
